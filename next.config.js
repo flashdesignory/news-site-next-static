278891e62367ec4d70b703dc34cd0918fd69dbaf
Thorsten Kober <tkober@gmail.com>
2023-05-24 14:00:50 -0700

    add asset prefix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
     reactStrictMode: true,
     output: "export",
     distDir: "docs",
-    assetPrefix: "./",
+    assetPrefix: debug ? "./" : "https://flashdesignory.github.io/news-site-next-static/",
     images: {
         unoptimized: true,
     }
