052994aa5e96b715b1ebf156fa59f29655a7db61
Thorsten Kober <tkober@gmail.com>
2023-06-06 16:16:43 -0700

    use next/image

---
+++ docs/_next/static/T-S2UcycXh5EClCxb3Z3k/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a0e07d66c78a79f7.css","static/chunks/743-828aa5d9397c7d08.js","static/chunks/pages/index-2944118bd1d1aaf1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
