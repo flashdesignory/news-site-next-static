66eb64ddcbe3f6c0ae720945028024c826b664b3
Thorsten Kober <tkober@gmail.com>
2023-06-08 12:07:25 -0700

    remove whitespaces

--- docs/_next/static/GrEbBRMm_GxJ5pb2kgvLF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/13a0cd264c4e50c7.css","static/chunks/743-828aa5d9397c7d08.js","static/chunks/pages/index-0308c21d5cbbc5a4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
