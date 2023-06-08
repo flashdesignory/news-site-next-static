637e72b62388a7f5a50d54aa3c4b1ad2aee0726b
Thorsten Kober <tkober@gmail.com>
2023-06-08 12:05:12 -0700

    cleanup

--- src/data/dialog.js
+++ src/data/dialog.js
@@ -1,15 +1,8 @@
 export const settings = {
-<<<<<<< HEAD
     header: "إعدادات",
     items: {
         motion: {
             label: "انخفاض الحركة"
-=======
-    header: "Settings",
-    items: {
-        motion: {
-            label: "Reduced Motion"
->>>>>>> main
         }
     }
 }
