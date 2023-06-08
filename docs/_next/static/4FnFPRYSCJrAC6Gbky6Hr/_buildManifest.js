eae7585a2e8d82f8e5b50b91b7bbf0ba09811dc0
Thorsten Kober <tkober@gmail.com>
2023-06-07 20:51:21 -0700

    fix activepath

---
+++ docs/_next/static/4FnFPRYSCJrAC6Gbky6Hr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/13a0cd264c4e50c7.css","static/chunks/743-828aa5d9397c7d08.js","static/chunks/pages/index-0cb13a5909e24fdf.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
