32b295723ee7520ad3cc7bb1c9c9e801e865c51b
Thorsten Kober <tkober@gmail.com>
2023-06-07 20:10:28 -0700

    add dialog translations

---
+++ docs/_next/static/y-rINGyVJXUiiG_f92UJU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/30c119993e11e905.css","static/chunks/743-828aa5d9397c7d08.js","static/chunks/pages/index-79c9904d748221ca.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
