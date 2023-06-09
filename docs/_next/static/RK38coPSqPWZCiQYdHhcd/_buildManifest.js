49afce6c650c51fcf936a886f445ac9d639feaba
Thorsten Kober <tkober@gmail.com>
2023-06-09 14:44:41 -0700

    add skip

---
+++ docs/_next/static/RK38coPSqPWZCiQYdHhcd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d443dbbd72838193.css","static/chunks/305-cc0e0a3eee16f989.js","static/chunks/pages/index-fcba3a78bf460373.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
