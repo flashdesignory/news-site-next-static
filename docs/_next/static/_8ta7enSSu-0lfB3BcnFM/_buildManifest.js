bf79ca37e5c1afd80f0bd04047e5e7785648c35a
Thorsten Kober <tkober@gmail.com>
2023-07-07 07:39:55 -0700

    fix toggle

--- docs/_next/static/_8ta7enSSu-0lfB3BcnFM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a4ac3b5de877b5bb.css","static/chunks/327-86ab067341d02e44.js","static/chunks/pages/index-125be3f6e7d7ba67.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
