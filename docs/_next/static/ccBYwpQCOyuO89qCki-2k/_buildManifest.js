b802db57d9d320a06d0780924f3ec68b25672e01
Thorsten Kober <tkober@gmail.com>
2023-07-10 13:10:36 -0700

    ids

--- docs/_next/static/ccBYwpQCOyuO89qCki-2k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5f360aec95c89274.css","static/chunks/327-86ab067341d02e44.js","static/chunks/pages/index-5543d9351b4c8188.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
