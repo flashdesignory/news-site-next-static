52f311addb1cd6d59c0492df7a49f97fe9f6afb5
Thorsten Kober <tkober@gmail.com>
2023-08-02 08:44:36 -0700

    ajust header

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ export default function App() {
             <Head>
                 <title>The Daily Broadcast</title>
                 <meta name="description" content="A news site developed with Next.js." key="description" />
+                <meta http-equiv="Permissions-Policy" content="interest-cohort=()" />
             </Head>
             <DataContextProvider>
                 <Router>
